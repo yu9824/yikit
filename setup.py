22f9b38b223db8e5513182a8c0d172a99f88b71d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-12-28 22:12:29 +0900

    0.0.2 make __init__.py. caution: package name of boruta is different from pip and conda.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='wrapper_method',
-    version='0.0.1',
+    version='0.0.2',
     description='for wrapper method. (by using boruta_py)',
     author='yu-9824',
     author_email='yu.9824@gmail.com',
