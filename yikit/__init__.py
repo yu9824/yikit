e7e490c87e5e09cb1e6d6e2380a9645e18580aaf
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-15 18:33:44 +0900

    v0.1.1 because of a mistake of requirements.txt

--- yikit/__init__.py
+++ yikit/__init__.py
@@ -4,7 +4,7 @@
 from .models import *
 from .tools import *
 
-__version__      = '0.1.0'
+__version__      = '0.1.1'
 __license__      = 'Apache License 2.0'
 __author__       = 'yu9824'
 __copyright__    = 'Copyright © 2021 yu9824'
