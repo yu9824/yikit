1e5c6ad0641dfce38bcc7fc03a4c09ada236df6c
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-14 18:30:44 +0900

    v0.3.11dev Add Datetime key

--- yikit/__init__.py
+++ yikit/__init__.py
@@ -1,7 +1,7 @@
 
 # 正規表現でtextファイルと同じやり方で読み込むのですべて手打ちする．
 
-__version__      = '0.3.7'
+__version__      = '0.3.11dev'
 __license__      = 'Apache License 2.0'
 __author__       = 'yu9824'
 __copyright__    = 'Copyright © 2021 yu9824'
