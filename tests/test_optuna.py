e53b6da875a0c308f5d88099bf6e83876da870e8
yu9824 <58211916+yu9824@users.noreply.github.com>
2026-01-28 07:22:32 +0900

    test: Update abs_tol

--- tests/test_optuna.py
+++ tests/test_optuna.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import KFold
 
 from yikit.models import Objective, ParamDistributions
 
+BEST_SCORE = -65.6
+ABS_TOL = 0.2
+
 
 def test_create_study(X_regression, y_regression):
     x = X_regression
@@ -26,7 +29,7 @@ def test_create_study(X_regression, y_regression):
     )
     study.optimize(objective, n_trials=10)
 
-    assert math.isclose(study.best_value, -65.6, abs_tol=0.1)
+    assert math.isclose(study.best_value, BEST_SCORE, abs_tol=ABS_TOL)
     assert study.best_trial.number == 3
 
 
@@ -47,5 +50,5 @@ def test_optuna_search_cv(X_regression, y_regression):
         random_state=334,
     )
     ocv.fit(x, y)
-    assert math.isclose(ocv.best_score_, -65.6, abs_tol=0.1)
+    assert math.isclose(ocv.best_score_, BEST_SCORE, abs_tol=ABS_TOL)
     assert ocv.study_.best_trial.number == 3
